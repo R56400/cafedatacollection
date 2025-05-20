acc20a9b7570fcbf20d4b1b824fe9375a9c4819a
Chris Jordan <chris.a.jordan@gmail.com>
2025-05-20 15:03:04 -0700

    back to cafe review

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
 import asyncio
 
-from articles.article import ArticlePipeline
-
-
-async def main():
-    # Initialize the pipeline with an input file
-    pipeline = ArticlePipeline("articles/input/input.json")
-    # Run the pipeline
-    await pipeline.run_pipeline()
-
+from cafe_data_collection.run_pipeline import main
 
 if __name__ == "__main__":
     asyncio.run(main())
