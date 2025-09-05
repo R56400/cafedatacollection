ca878d34812ed9e6fa62b430d75ce9366ff1fffb
Chris Jordan <chris.a.jordan@gmail.com>
2025-09-05 12:54:46 -0700

    max tokens fix

--- cafe_data_collection/llm_client.py
+++ cafe_data_collection/llm_client.py
@@ -203,7 +203,7 @@ class LLMClient:
                             "model": self.model,
                             "messages": messages,
                             "temperature": self.temperature,
-                            "max_tokens": self.max_tokens,
+                            "max_completion_tokens": self.max_tokens,
                         },
                         timeout=90.0,
                     )
